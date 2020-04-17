f6a949ae7e8467e005d80cf79ec7cc6c9d7202b4
Dabble <ddabble@users.noreply.github.com>
2020-04-17 17:13:58 +0200

    Improved Project and User admin pages

--- projects/admin.py
+++ projects/admin.py
@@ -1,27 +1,60 @@
 from django.contrib import admin
 
-from .models import Delivery, Project, ProjectCategory, ProjectCategoryProposal, Task, TaskFile, TaskFileTeam, Team
-
-
-class TaskInline(admin.TabularInline):
-    model = Task
-    verbose_name_plural = 'Tasks'
+from util.utils import HtmlUtils
+from .models import Delivery, Project, ProjectCategory, ProjectCategoryProposal, Task, TaskFile, TaskFileTeam, TaskOffer, Team
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    inlines = (TaskInline,)
+    class TaskInline(admin.TabularInline):
+        model = Task
+        verbose_name_plural = 'Tasks'
+        extra = 0
 
-    def get_inline_instances(self, request, obj=None):
-        if not obj:
-            return super().get_inline_instances(request, obj)
-        return []
+        def get_queryset(self, request):
+            qs = super().get_queryset(request)
+            return qs.prefetch_related('read__user', 'write__user', 'modify__user')
+
+    inlines = [TaskInline]
+
+    list_display = ('title', 'user_profile', 'get_tasks', 'get_budgets', 'get_offers', 'get_participants', 'category', 'status')
+    list_filter = ('category', 'status')
+    search_fields = ('title', 'description', 'tasks__title', 'user_profile__user__username', 'participants__user__username')
+    ordering = ['user_profile', 'title']
+
+    raw_id_fields = ('user_profile',)
+    filter_horizontal = ('participants',)
+
+    def get_tasks(self, project):
+        return HtmlUtils.block_join(project.tasks.all(), sep=HtmlUtils.EN_DASH)
+
+    get_tasks.short_description = 'Tasks'
+
+    def get_budgets(self, project):
+        return HtmlUtils.block_join([t.budget for t in project.tasks.all()], sep=HtmlUtils.EN_DASH)
+
+    get_budgets.short_description = 'Budgets'
+
+    def get_offers(self, project):
+        return HtmlUtils.block_join([len(t.offers.all()) for t in project.tasks.all()], sep=HtmlUtils.EN_DASH)
+
+    get_offers.short_description = 'Offers'
+
+    def get_participants(self, project):
+        return HtmlUtils.block_join(project.participants.all())
+
+    get_participants.short_description = 'Participants'
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('category').prefetch_related('tasks__offers', 'participants')
 
 
-admin.site.register(Project, ProjectAdmin)
-admin.site.register(Task)
-admin.site.register(TaskFile)
-admin.site.register(Delivery)
 admin.site.register(ProjectCategory)
 admin.site.register(ProjectCategoryProposal)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task)
 admin.site.register(Team)
+admin.site.register(TaskFile)
 admin.site.register(TaskFileTeam)
+admin.site.register(Delivery)
+admin.site.register(TaskOffer)
--- user/admin.py
+++ user/admin.py
@@ -1,34 +1,52 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.db.models.functions import Concat
 
 from .models import Profile
 
 
-class ProfileInline(admin.StackedInline):
-    model = Profile
-    can_delete = False
-    verbose_name_plural = 'Profile'
-    fk_name = 'user'
-
-
 class CustomUserAdmin(UserAdmin):
+    class ProfileInline(admin.StackedInline):
+        model = Profile
+        can_delete = False
+        verbose_name_plural = 'Profile'
+        fk_name = 'user'
+
     inlines = (ProfileInline,)
 
-    list_display = ('username', 'email', 'first_name', 'last_name', 'get_company', 'is_active')
+    #                                    Removes 'is_staff'
+    list_display = UserAdmin.list_display[:-1] + ('get_company', 'is_active')
+    list_select_related = ('profile',)
     list_editable = ('is_active',)
 
-    def get_inline_instances(self, request, obj=None):
-        if not obj:
-            return []
-        return super().get_inline_instances(request, obj)
+    def get_company(self, user):
+        return user.profile.company
 
-    def get_company(self, obj):
-        return obj.profile.company
-
-    get_company.admin_order_field = 'company'
+    get_company.admin_order_field = 'profile__company'
     get_company.short_description = 'Company'
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Only used by `raw_id_fields` in other model admins.
+    """
+    list_display = ('user', 'get_full_name', 'company', 'country', 'state', 'city')
+    list_select_related = ('user',)
+    list_filter = ('company', 'country')
+    search_fields = [f'user__{f}' for f in UserAdmin.search_fields] + ['company']
+
+    def get_full_name(self, profile):
+        return profile.user.get_full_name()
+
+    get_full_name.admin_order_field = Concat('user__first_name', 'user__last_name')
+    get_full_name.short_description = 'Full name'
+
+    def get_model_perms(self, request):
+        # Hides model admin from admin list
+        return {}
 
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile, ProfileAdmin)
