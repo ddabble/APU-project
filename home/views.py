5e09ccf53483ce30222c06a772096b9297c8864b
Dabble <ddabble@users.noreply.github.com>
2020-04-18 04:52:09 +0200

    Refactored home view

--- home/views.py
+++ home/views.py
@@ -4,33 +4,16 @@ from projects.models import Project, ProjectCategoryProposal
 
 
 def home(request):
-    if request.user.is_authenticated:
-        user = request.user
-        user_projects = Project.objects.filter(user_profile=user.profile)
-        customer_projects = list(Project.objects.filter(participants__id=user.id))
-        for team in user.profile.teams.select_related('task__project'):
-            customer_projects.append(team.task.project)
-        cd = {}
-        for customer_project in customer_projects:
-            cd[customer_project.id] = customer_project
-
-        customer_projects = cd.values()
-        given_offers_projects = get_given_offer_projects(user)
-        return render(request, 'index.html', {
-            'user_projects':          user_projects,
-            'customer_projects':      customer_projects,
-            'given_offers_projects':  given_offers_projects,
-            'num_category_proposals': ProjectCategoryProposal.objects.count(),
-            'Project':                Project,
-        })
-    else:
+    if not request.user.is_authenticated:
         return redirect('projects')
 
-
-def get_given_offer_projects(user):
-    projects = set()
-
-    for taskoffer in user.profile.task_offers.select_related('task__project'):
-        projects.add(taskoffer.task.project)
-
-    return projects
+    profile = request.user.profile
+    customer_projects = profile.participating_projects.all()
+    team_projects = Project.objects.filter(tasks__teams__members=profile)
+    return render(request, 'index.html', {
+        'user_projects':          profile.projects.all(),
+        'customer_projects':      set(customer_projects) | set(team_projects),
+        'given_offers_projects':  Project.objects.filter(tasks__offers__offerer=profile).distinct(),
+        'num_category_proposals': ProjectCategoryProposal.objects.count(),
+        'Project':                Project,
+    })
